8c627c10905490de2c73a69cb3f9d441feb73e50
unpolochon <pnirina@hotmail.com>
2025-05-13 23:30:47 +0200

    draw player and manage right and left turn

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
+        # set the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
